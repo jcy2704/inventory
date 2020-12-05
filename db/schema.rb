d869ec2850bbbf41af1eede9bd875f0c860a0dae
Steven Jack Chung <stevenjchung12@gmail.com>
2020-12-05 14:58:35 -0500

    Add Transaction model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_204556) do
+ActiveRecord::Schema.define(version: 2020_12_05_195743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 2020_12_03_204556) do
     t.index ["group_id"], name: "index_products_on_group_id"
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_transactions_on_product_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "role", default: "employee", null: false
@@ -64,4 +73,6 @@ ActiveRecord::Schema.define(version: 2020_12_03_204556) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "products", "groups"
+  add_foreign_key "transactions", "products"
+  add_foreign_key "transactions", "users"
 end
