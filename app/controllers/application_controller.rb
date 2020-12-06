b1effb08685dde1570f82690bfea7b27b90cf33b
Steven Jack Chung <stevenjchung12@gmail.com>
2020-12-06 14:32:34 -0500

    Add button to product view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?, :exists?, :redirect_if_not_logged
   add_flash_types :succeeded
 
+  before_action :current_cart
+
   def current_user
     @current_user ||= User.find(session[:current_user]['id']) if session[:current_user]
   end
@@ -18,5 +20,23 @@ class ApplicationController < ActionController::Base
   def redirect_if_not_logged
     exists?
     redirect_to login_path unless logged_in?
+  end
+
+  private
+
+  def current_cart
+    if session[:cart_id]
+      cart = Cart.find(id: session[:cart_id])
+      if cart.present?
+        @current_cart = cart
+      else
+        session[:cart_id] = nil
+      end
+    end
+
+    if session[:cart_id].nil?
+      @current_cart = Cart.create
+      session[:cart_id] = @current_cart.id
+    end
   end
 end
