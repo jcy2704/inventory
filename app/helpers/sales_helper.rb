b9e9b01afce78a381931c9c69bfb592361f4c85a
Steven Jack Chung <stevenjchung12@gmail.com>
2020-12-07 19:09:58 -0500

    Add simple structure for sales cards

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,2 +1,9 @@
 module SalesHelper
+  def sales_group_icon(item)
+    if !item.product.group.icon.attached?
+      image_tag 'category.svg', class: 'w-100'
+    else
+      image_tag item.product.group.icon, class: 'w-100'
+    end
+  end
 end
