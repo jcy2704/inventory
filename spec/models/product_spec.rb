f0cb1e53acf42375b62ad1dd6a8f1c8fcee49547
Steven Jack Chung <stevenjchung12@gmail.com>
2020-12-08 12:38:31 -0500

    Add tests to sales and product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it do
+    Group.create(name: 'Group')
+    Product.create(name: 'Product', quantity: 2, price: 12, group_id: 1)
+    should validate_uniqueness_of(:name)
+  end
+
+  it { should validate_presence_of(:price) }
+  it do
+    should validate_numericality_of(:price)
+      .is_greater_than_or_equal_to(0)
+  end
+
+  it { should validate_presence_of(:quantity) }
+  it do
+    should validate_numericality_of(:quantity)
+      .only_integer.is_greater_than_or_equal_to(0)
+  end
+
+  it { should validate_presence_of(:group_id) }
+
+  it { should belong_to(:group) }
+
+  it { should have_many(:line_items) }
 end
