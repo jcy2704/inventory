ac7c39ecf1de8e0973c5d158f273b8f6ed4eb040
Steven Jack Chung <stevenjchung12@gmail.com>
2020-12-09 12:20:43 -0500

    Move logic from cart to helpers

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,19 @@
 module CartsHelper
+  def empty_cart(cart)
+    return unless cart.line_items != []
+
+    '<div class="add-product delete-product">'.html_safe +
+      (link_to 'Empty cart', cart_path(@current_cart), method: :delete, data: { confirm: 'Are you sure?' }) +
+      '</div>'.html_safe
+  end
+
+  def footer_cart(cart)
+    return unless cart.line_items != []
+
+    '<div class="flex footer"><div class="sub-total"><h3>Sub total: '.html_safe +
+      number_to_currency(cart.sub_total).to_s +
+      '</h3><div class="sell-btn">'.html_safe +
+      (button_to 'Sell', sales_path, method: :post) +
+      '</div></div></div>'.html_safe
+  end
 end
